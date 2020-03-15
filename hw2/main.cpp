56e02594fadb8bf6069e9df5b6e6638f0a261023
heliumyc <ericyc4@gmail.com>
2020-03-15 02:54:35 -0400

    test omp main

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -2,3 +2,9 @@
 // Created by CONG YU on 2020/3/8.
 //
 
+#include <omp.h>
+#include <stdio.h>
+int main() {
+#pragma omp parallel
+    printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
+}
