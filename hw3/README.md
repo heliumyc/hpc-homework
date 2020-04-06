6820dd126fc46409c2b0932ebc957c2f5d71a618
heliumyc <ericyc4@gmail.com>
2020-04-06 12:51:41 -0400

    init

--- hw3/README.md
+++ hw3/README.md
@@ -1,1 +1,32 @@
-# HPCSpring2020
+# homework 3
+
+## OMP SCAN
+
+benchmark with different threads
+
+
+|    Threads   | Sequential | Parallel |
+| ----------- | ----------- | ----------- |
+| 1      | 0.414558s       |   0.637962s    |
+| 2   | 0.400608s        | 0.546453s  |
+| 3   | 0.393964s        | 0.240287s  |
+| 4   | 0.397146s        | 0.205412s  |
+| 5   | 0.395476s        |  0.189222s |
+| 6   | 0.390131s        |  0.168190s |
+| 7   | 0.393331s        |  0.239960s |
+| 8   | 0.392442s        | 0.252725s  |
+
+We can see that parallel code works best with 8 threads
+
+Architecture
+```text
+Architecture:  x86_64
+Processor Name: 6-Core Intel Core i7
+Processor Speed: 2.2 GHz 
+Number of processors: 1 
+Total number of cores: 6 
+L2 cache (per core): 256 KB 
+L3 cache: 9 MB 
+Hyper-Threading Technology: Enabled    
+Memory: 16 GB 
+```
--- hw3/omp-scan.cpp
+++ hw3/omp-scan.cpp
@@ -15,6 +15,41 @@ void scan_seq(long* prefix_sum, const long* A, long n) {
 
 void scan_omp(long* prefix_sum, const long* A, long n) {
   // TODO: implement multi-threaded OpenMP scan
+  int p = 6;
+  long len = (n-1)/p + ((n-1)%p > 0);
+  long* partial_sum = (long*) malloc(p* sizeof(long));
+  std::fill(partial_sum, partial_sum+p, 0);
+  prefix_sum[0] = 0;
+  omp_set_num_threads(p);
+
+#pragma omp parallel for default(shared)
+    {
+        for (int k = 0; k < p; k++) {
+            prefix_sum[len * k + 1] = A[len * k];
+            int i = len * k + 2;
+            for (; i < n && i <= len * (k + 1); i++) {
+                prefix_sum[i] = prefix_sum[i - 1] + A[i - 1];
+            }
+            partial_sum[k] = prefix_sum[i - 1];
+        }
+    }
+
+#pragma omp single
+            for (int k = 1; k < p; k++) {
+                partial_sum[k] += partial_sum[k - 1];
+            }
+
+#pragma omp parallel for default(shared)
+    {
+        for (int k = 1; k < p; k++) {
+            for (long i = len * k + 1; i < n && i <= len * (k + 1); i++) {
+                prefix_sum[i] += partial_sum[k - 1];
+            }
+        }
+    }
+
+    free(partial_sum);
+
 }
 
 int main() {
@@ -22,7 +57,7 @@ int main() {
   long* A = (long*) malloc(N * sizeof(long));
   long* B0 = (long*) malloc(N * sizeof(long));
   long* B1 = (long*) malloc(N * sizeof(long));
-  for (long i = 0; i < N; i++) A[i] = rand();
+    for (long i = 0; i < N; i++) A[i] = rand();
 
   double tt = omp_get_wtime();
   scan_seq(B0, A, N);
