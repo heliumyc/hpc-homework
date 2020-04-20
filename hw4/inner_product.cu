fb95f2126820dec76c830b7c0f4916f1160f4648
heliumyc <ericyc4@gmail.com>
2020-04-20 03:40:21 -0400

    save

--- hw4/inner_product.cu
+++ hw4/inner_product.cu
@@ -56,7 +56,7 @@ void gpu_map_vec_inner_product(const double*a, const double *b, double *c, long 
 
 // gpu reduce
 __global__
-void gpu_reduce_inner_product(const double *a, double *sum, long n) {
+void gpu_reduce_inner_product_kernal1(const double *a, double *sum, long n) {
     __shared__ double smem[BLOCK_SIZE];
     int idx = (blockIdx.x) * blockDim.x + threadIdx.x;
 
@@ -77,6 +77,25 @@ void gpu_reduce_inner_product(const double *a, double *sum, long n) {
     // write to global memory
     if (threadIdx.x == 0) sum[blockIdx.x] = smem[threadIdx.x];
 
+}
+
+__global__ void reduction_kernel0(double* sum, const double* a, long N){
+    __shared__ double smem[BLOCK_SIZE];
+    int idx = (blockIdx.x) * blockDim.x + threadIdx.x;
+
+    // each thread reads data from global into shared memory
+    if (idx < N) smem[threadIdx.x] = a[idx];
+    else smem[threadIdx.x] = 0;
+    __syncthreads();
+
+    for(int s = 1; s < blockDim.x; s *= 2) {
+        if(threadIdx.x % (2*s) == 0)
+            smem[threadIdx.x] += smem[threadIdx.x + s];
+        __syncthreads();
+    }
+
+    // write to global memory
+    if (threadIdx.x == 0) sum[blockIdx.x] = smem[threadIdx.x];
 }
 
 int main() {
@@ -158,11 +177,11 @@ int main() {
     gpu_map_vec_inner_product<<<n/BLOCK_SIZE,BLOCK_SIZE>>>(a_d, b_d, temp, n);
 
     long Nb = (n+BLOCK_SIZE-1)/(BLOCK_SIZE);
-    gpu_reduce_inner_product<<<Nb,BLOCK_SIZE>>>(sum_d, temp_d, n);
+    reduction_kernel0<<<Nb,BLOCK_SIZE>>>(sum_d, temp_d, n);
     while (Nb > 1) {
         long lastN = Nb;
         Nb = (Nb+BLOCK_SIZE-1)/(BLOCK_SIZE);
-        gpu_reduce_inner_product<<<Nb,BLOCK_SIZE>>>(sum_d + lastN, sum_d, lastN);
+        reduction_kernel0<<<Nb,BLOCK_SIZE>>>(sum_d + lastN, sum_d, lastN);
         sum_d += lastN;
     }
     double cuda_res;
