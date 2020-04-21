f0556606a52bad327fe798852b70593338ee4e88
heliumyc <ericyc4@gmail.com>
2020-04-20 23:21:58 -0400

    save

--- hw4/vec_mat_mul.cu
+++ hw4/vec_mat_mul.cu
@@ -187,12 +187,11 @@ int main() {
         Check_CUDA_Error("error");
         printf("there is the problem\n");
     }
-    Check_CUDA_Error("loop finish");
 
     // fetch mul result
     // currently sum_d is out anwser
     printf("out of loop\n");
-    cudaMemcpyAsync(&vec_mul, sum_d-n, n * sizeof(double), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&vec_mul, sum_d-n, n * sizeof(double), cudaMemcpyDeviceToHost);
     Check_CUDA_Error("copy result back failed");
     cudaDeviceSynchronize();
 
