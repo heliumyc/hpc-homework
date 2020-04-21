e175a154c075820f7b362f98d92ad5be2b5e7dec
heliumyc <ericyc4@gmail.com>
2020-04-21 13:34:02 -0400

    save

--- hw4/vec_mat_mul.cu
+++ hw4/vec_mat_mul.cu
@@ -39,7 +39,7 @@ void openmp_vec_mat_mul(double *res, const double *mat, const double *vec, long 
     }
 }
 
-#define BLOCK_SIZE 256
+#define BLOCK_SIZE 64
 
 __device__ double atomicAdd2(double* address, double val)
 {
