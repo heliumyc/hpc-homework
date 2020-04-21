9d0e64d97f03ef58bf969affd44e6186573c5ec8
heliumyc <ericyc4@gmail.com>
2020-04-21 17:40:14 -0400

    save

--- hw4/jacobi.cu
+++ hw4/jacobi.cu
@@ -53,7 +53,10 @@ long jacobi_cpu(double* u, double* v) {
                 v[i*SIZE+j] = (hSqr+u[(i-1)*SIZE+j]+u[i*SIZE+j-1]+u[(i+1)*SIZE+j]+u[i*SIZE+j+1])/4;
             }
         }
-        std::swap(*u, *v);
+        double* temp = u;
+        u = v;
+        v = temp;
+//        std::swap(*u, *v);
 
         if (initResidual/curResidual > 1e+6) {
             printf("%lf\n", curResidual);
