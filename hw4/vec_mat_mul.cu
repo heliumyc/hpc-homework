90377e97944d19ca8484e04533615a0562664ab1
heliumyc <ericyc4@gmail.com>
2020-04-20 23:27:24 -0400

    save

--- hw4/vec_mat_mul.cu
+++ hw4/vec_mat_mul.cu
@@ -158,7 +158,7 @@ int main() {
     double *extra_d;
     long N_work = 1;
     for (long i = (n + BLOCK_SIZE - 1) / (BLOCK_SIZE); i > 1; i = (i + BLOCK_SIZE - 1) / (BLOCK_SIZE)) N_work += i;
-    cudaMalloc(&extra_d, (N_work+2) * n * sizeof(double)); // extra memory buffer for reduction across thread-blocks
+    cudaMalloc(&extra_d, (N_work+BLOCK_SIZE) * n * sizeof(double)); // extra memory buffer for reduction across thread-blocks
     cudaDeviceSynchronize();
 
 
